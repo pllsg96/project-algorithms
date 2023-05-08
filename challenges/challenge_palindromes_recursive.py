9210ab74aa3e1b65aa09521ac869a8cff803c184
Pedro Luiz <pedroluizlsg@gmail.com>
2023-05-08 10:36:34 -0300

    feat: implementação função is_palindrome_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,22 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """Faça o código aqui."""
-    raise NotImplementedError
+    print(low_index, high_index)
+
+    if (len(word) == 0):
+        return False
+
+    elif (low_index >= high_index):
+        return True
+
+    elif (word[low_index] != word[high_index]):
+        return False
+
+    elif is_palindrome_recursive(word, low_index + 1, high_index - 1):
+        return True
+
+    return False
+
+
+if __name__ == "__main__":
+    x = is_palindrome_recursive('TENET', 0, 4)
+    print(x)
