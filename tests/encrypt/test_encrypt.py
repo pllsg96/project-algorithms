5ac9277b546230d7be53fb53480baca9152bfaa2
Pedro Luiz <pedroluizlsg@gmail.com>
2023-05-10 10:45:59 -0300

    feat: implementação teste para encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message("a", "b")
+
+    with pytest.raises(TypeError):
+        encrypt_message(1, 2)
+
+    assert encrypt_message("pizza", 0) == "azzip"
+    assert encrypt_message("champignon", 3) == "ahc_nongipm"
+    assert encrypt_message("calabresa", 4) == "aserb_alac"
