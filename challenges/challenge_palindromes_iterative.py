53097450c23f077753f39b06d2ad24be0f8cb25f
Pedro Luiz <pedroluizlsg@gmail.com>
2023-05-10 12:34:13 -0300

    feat: implementação da função is_palindrome_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,21 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+
+    if (len(word) <= 0):
+        return False
+
+    low_index = 0
+    high_index = len(word) - 1
+
+    while low_index < high_index:
+        if word[low_index] != word[high_index]:
+            return False
+
+        low_index += 1
+        high_index -= 1
+
+    return True
+
+
+if __name__ == "__main__":
+    x = is_palindrome_iterative('TENET')
+    print(x)
